5f24f1318b90349db6a295d893ed0d6c30d1afc1
Do1e <dpj.email@qq.com>
2022-08-15 16:52:07 +0800

    add getconsItems

--- api.py
+++ api.py
@@ -90,7 +90,7 @@ def getRooms(save: bool) -> dict:
 
 def getScenes(save: bool, roomIdx=0) -> dict:
 	"""
-	getScenes(save) -> dict.
+	getScenes(save, roomIdx=0) -> dict.
 	@description:
 	获取场景列表
 	-------
@@ -157,6 +157,38 @@ def runScene(name: str) -> int:
 		print(msg)
 		return -1
 
+def getconsItems(save: bool, roomIdx=0) -> dict:
+	"""
+	getconsItems(save, roomIdx=0) -> dict.
+	@description:
+	获取耗材列表
+	-------
+	@param:
+	save: bool, 是否保存到文件./json/consItems.json中
+	roomIdx: int, 房间索引(在./json/rooms.json中找到对应的房间索引，默认为0)
+	-------
+	@Returns:
+	dict, 耗材列表的字典
+	-------
+	"""
+	authorize = json.load(open('./json/authorize.json', 'r', encoding='utf-8'))
+	if os.path.exists('./json/rooms.json'):
+		rooms = json.load(open('./json/rooms.json', 'r', encoding='utf-8'))
+	else:
+		rooms = getRooms(save)
+	try:
+		homeId = rooms['result']['homelist'][roomIdx]['id']
+	except IndexError:
+		print('房间号超出范围')
+		return
+	data = {"home_id": int(homeId), "owner_id": authorize['userId']}
+	msg = postData('/v2/home/standard_consumable_items', data, authorize)
+	consItems = json.loads(msg)
+	if save:
+		with open('./json/consItems.json', 'w') as f:
+			f.write(json.dumps(consItems, indent=4, ensure_ascii=False))
+	return consItems
+
 def getDevAtt(devs: list) -> dict:
 	"""
 	getDevAtt(devs) -> dict.
@@ -209,6 +241,10 @@ if __name__ == '__main__':
 
 	# 执行手动场景
 	# runScene('控制台灯')
+
+	# 获取设备耗材
+	# consItems = getconsItems(True)
+	# print(consItems)
 
 	# 参数说明
 	# did: 设备ID
