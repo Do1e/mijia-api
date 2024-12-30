314d03f325576408619bf7cdd4aa8910bae097b8
Do1e <dpj.email@qq.com>
2024-12-30 16:59:41 +0800

    chore: use poetry

--- mijiaAPI/__init__.py
+++ mijiaAPI/__init__.py
@@ -1,13 +1,3 @@
 from .login import mijiaLogin
 from .apis import mijiaAPI
 from .devices import mijiaDevices
-
-from .__version__ import (
-    __title__,
-    __version__,
-    __description__,
-    __url__,
-    __author__,
-    __author_email__,
-    __license__,
-)
--- mijiaAPI/__version__.py
+++
@@ -1,7 +0,0 @@
-__title__ = "mijiaAPI"
-__version__ = "1.2.0"
-__description__ = "A Python API for Xiaomi Mijia"
-__url__ = "https://github.com/Do1e/mijia-api"
-__author__ = "Do1e"
-__author_email__ = "dpj.email@qq.com"
-__license__ = "GPLv3"
